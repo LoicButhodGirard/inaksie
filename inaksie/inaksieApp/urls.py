9a33a660a27a71fac93be9c9e235b6b904e8ab40
UWKM <uwkm@145-138-65-67.aventus.nl>
2022-05-18 10:43:14 +0200

    added all template files

--- inaksie/inaksieApp/urls.py
+++ inaksie/inaksieApp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('porfolio/', views.portfolio, name='porfolio'),
+    path('contact/', views.contact, name='contact'),
+    path('werkwijze/', views.werkwijze, name='werkwijze'),
+    path('intro/', views.intro, name='intro'),
 ]
--- inaksie/inaksieApp/views.py
+++ inaksie/inaksieApp/views.py
@@ -3,3 +3,19 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'index.html')
+
+
+def contact(request):
+    return render(request,'contact.html')
+
+
+def intro(request):
+    return render(request,'intro.html')
+
+
+def portfolio(request):
+    return render(request,'portfolio.html')
+
+
+def werkwijze(request):
+    return render(request,'werkwijze.html')
